624316c951194406d167c0665b43db6854cdedfa
Nick McFaul <2142579-nick.mcfaul@users.noreply.gitlab.com>
2021-06-10 00:56:56 -0400

    update terraform provider configuration
    Signed-off-by: Nick McFaul <2142579-nick.mcfaul@users.noreply.gitlab.com>

--- infrastructure/terraform/azure/provider.tf
+++ infrastructure/terraform/azure/provider.tf
@@ -1,10 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
 provider "azurerm" {
-  version = "=2.62.1"
   features {}
 }
-provider "azuread" { version = "1.0.0" }
-provider "random" { version = "3.0.0" }
-provider "time" { version = "0.6.0" }
+provider "azuread" {}
+provider "random" {}
+provider "time" {}
 
 data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
