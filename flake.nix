25c6631fb5d72d803be6dc58e8b692f8afdfb8b3
Nick M <4718+rkage@users.noreply.github.com>
2023-12-25 09:57:36 -0500

    feat: add nix flake environment config

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "manage home-cluster environment with nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    # The shell environment is intended for all systems
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          kubectl
+          kubectl-cnpg
+          kubectl-view-secret
+          talosctl
+          kubernetes-helm
+          fluxcd
+          cilium-cli
+          stern
+          terraform
+          go-task
+        ];
+        shellHook = ''
+          task
+        '';
+      };
+    });
+}
